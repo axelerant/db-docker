50b46262d78df82e0c694d17aa652df8478906f1
hw <hussainweb@gmail.com>
2020-06-29 13:14:14 -0400

    Enforce PSR-12 standards

--- src/DbDockerCommand.php
+++ src/DbDockerCommand.php
@@ -23,45 +23,71 @@ class DbDockerCommand extends BaseCommand
      */
     protected $output;
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configure()
     {
         $this->setName('db-docker')
             ->setDescription('Generate a Docker image for the database.')
-            ->addOption('docker-tag', 't', InputOption::VALUE_OPTIONAL,
-                'The Docker tag to build')
-            ->addOption('git-remote', 'r', InputOption::VALUE_OPTIONAL,
-                'The git remote to use to determine the image name', 'origin')
-            ->addOption('db-source', 's', InputOption::VALUE_OPTIONAL,
-                'Source of the database ("lando", "drush", or "file")')
-            ->addOption('db-file', 'f', InputOption::VALUE_OPTIONAL,
-                'The path to the database file (required if db-source is set to file)')
-            ->addOption('no-push', NULL, InputOption::VALUE_NONE,
-                'Set to not push the image after building');
+            ->addOption(
+                'docker-tag',
+                't',
+                InputOption::VALUE_OPTIONAL,
+                'The Docker tag to build'
+            )
+            ->addOption(
+                'git-remote',
+                'r',
+                InputOption::VALUE_OPTIONAL,
+                'The git remote to use to determine the image name',
+                'origin'
+            )
+            ->addOption(
+                'db-source',
+                's',
+                InputOption::VALUE_OPTIONAL,
+                'Source of the database ("lando", "drush", or "file")'
+            )
+            ->addOption(
+                'db-file',
+                'f',
+                InputOption::VALUE_OPTIONAL,
+                'The path to the database file (required if db-source is set to file)'
+            )
+            ->addOption(
+                'no-push',
+                null,
+                InputOption::VALUE_NONE,
+                'Set to not push the image after building'
+            );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->input = $input;
         $this->output = $output;
 
-        // Determine the Docker image name.
         $imageId = $this->getImageId();
 
-        // Get the database file.
         $sqlFile = $this->getDbFile();
 
-        // Clone the dockerize-db repository so that we can build the image
         $this->buildImage($imageId, $sqlFile);
 
         if (!$this->input->getOption('no-push')) {
             $this->output->writeln("<info>Pushing image...</info>");
             $this->execCmd("docker push " . $imageId);
-        }
-        else {
+        } else {
             $this->output->writeln(sprintf("<info>To push image, run '%s'</info>", "docker push " . $imageId));
         }
     }
 
+    /**
+     * Get the complete image name (with tag) based on given options.
+     */
     protected function getImageId(): string
     {
         // We can safely use `getcwd()` even in a subdirectory.
@@ -69,14 +95,18 @@ class DbDockerCommand extends BaseCommand
         $tag = $this->input->getOption('docker-tag');
         if (!$tag) {
             $tag = $git->getMainBranch()->getName();
-            $this->output->writeln(sprintf("<info>Docker tag not specified. Using current branch name: %s</info>",
-                $tag));
+            $this->output->writeln(sprintf(
+                "<info>Docker tag not specified. Using current branch name: %s</info>",
+                $tag
+            ));
 
             // We should be using the tag 'latest' if the current branch is 'master'.
             if ($tag == 'master') {
                 $tag = 'latest';
-                $this->output->writeln("<info>Using Docker tag 'latest' for branch 'master'.</info>",
-                    OutputInterface::VERBOSITY_VERBOSE);
+                $this->output->writeln(
+                    "<info>Using Docker tag 'latest' for branch 'master'.</info>",
+                    OutputInterface::VERBOSITY_VERBOSE
+                );
             }
         }
 
@@ -87,6 +117,14 @@ class DbDockerCommand extends BaseCommand
         return $this->getImagePathFromRepoUrl($remote->getFetchURL(), $tag);
     }
 
+    /**
+     * Get the complete Docker image name (with tag) from the repo URL.
+     *
+     * @param string $url
+     * @param string $tag
+     *
+     * @return string
+     */
     protected function getImagePathFromRepoUrl(string $url, string $tag): string
     {
         if (!preg_match('/^[^@]*@([^:]*):(.*)\.git$/', $url, $matches)) {
@@ -110,6 +148,9 @@ class DbDockerCommand extends BaseCommand
         return sprintf("%s/%s/db:%s", $registryDomain, strtolower($path), $tag);
     }
 
+    /**
+     * Get the database file path based on the source.
+     */
     protected function getDbFile(): string
     {
         $src = $this->input->getOption('db-source') ?: $this->guessSource();
@@ -117,8 +158,10 @@ class DbDockerCommand extends BaseCommand
             throw new InvalidOptionException("db-source can only be 'lando', 'drush', or 'file'");
         }
 
-        $this->output->writeln(sprintf("<info>Getting SQL file from source '%s'</info>",
-            $src));
+        $this->output->writeln(sprintf(
+            "<info>Getting SQL file from source '%s'</info>",
+            $src
+        ));
 
         if ($src == 'file') {
             if (!$this->input->getOption('db-file')) {
@@ -138,6 +181,9 @@ class DbDockerCommand extends BaseCommand
         return $sqlFileName;
     }
 
+    /**
+     * Guess the best database source.
+     */
     protected function guessSource(): string
     {
         if ($this->input->getOption('db-file')) {
@@ -155,6 +201,8 @@ class DbDockerCommand extends BaseCommand
     }
 
     /**
+     * Build the image using our Dockerfile and SQL scripts
+     *
      * @param string $imageId
      * @param string $sqlFile
      */
@@ -170,27 +218,38 @@ class DbDockerCommand extends BaseCommand
         copy($sqlFile, $tempPath . "/dumps/db.sql");
         copy($assetPath . "zzzz-truncate-caches.sql", $tempPath . "zzzz-truncate-caches.sql");
 
-        $this->output->writeln(sprintf("<info>Building image '%s'</info>",
-            $imageId));
+        $this->output->writeln(sprintf(
+            "<info>Building image '%s'</info>",
+            $imageId
+        ));
         $dockerCmd = sprintf("docker build -t %s %s", $imageId, $tempPath);
         $this->execCmd($dockerCmd);
     }
 
     protected function execCmd($cmd): void
     {
-        $this->output->writeln(sprintf("<info>Running '%s'</info>", $cmd),
-            OutputInterface::VERBOSITY_VERBOSE);
+        $this->output->writeln(
+            sprintf("<info>Running '%s'</info>", $cmd),
+            OutputInterface::VERBOSITY_VERBOSE
+        );
         exec($cmd, $res, $code);
         if ($code != 0) {
-            $this->output->writeln(sprintf("<error>Command returned exit code '%d'</error>",
-                $code));
+            $this->output->writeln(sprintf(
+                "<error>Command returned exit code '%d'</error>",
+                $code
+            ));
         }
-        $this->output->writeln($res,
-            OutputInterface::OUTPUT_RAW | OutputInterface::VERBOSITY_VERBOSE);
+        $this->output->writeln(
+            $res,
+            OutputInterface::OUTPUT_RAW | OutputInterface::VERBOSITY_VERBOSE
+        );
 
         if ($code != 0) {
-            $msg = sprintf("Command returned exit code '%d'\n%s",
-                $code, implode("\n", $res));
+            $msg = sprintf(
+                "Command returned exit code '%d'\n%s",
+                $code,
+                implode("\n", $res)
+            );
             throw new RuntimeException($msg, $code);
         }
     }
